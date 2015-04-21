526a151df7b6c2bb4fa01db438f48cf26072552a
Carlos Flores <darthmincho@gmail.com>
2015-04-21 13:31:46 -0400

    Add simple Fabfile.

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from __future__ import with_statement
+from fabric.api import *
+from fabric.context_managers import shell_env
+
+env.user = 'django'
+env.hosts = ['hashmap.org']
+
+def deploy():
+	code_dir = '/home/django/RSVPBot'
+	with cd(code_dir):
+		run('git pull')
+		
