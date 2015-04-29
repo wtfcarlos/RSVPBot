cacf3687eb64577ea5c2d697aff560b0f2957462
Carlos Flores <darthmincho@gmail.com>
2015-04-29 12:29:35 -0400

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,10 @@ from fabric.api import *
 from fabric.context_managers import shell_env
 
 env.user = 'django'
-env.hosts = ['hashmap.org']
+env.hosts = ['insomn.io']
 
 def deploy():
-	local('git add .')
-	local('git commit -m "Fabric auto deploy"')
-	local('git push origin master')
 	code_dir = '/home/django/RSVPBot'
 	with cd(code_dir):
 		run('git pull')
+		run('sudo service rsvp restart')
